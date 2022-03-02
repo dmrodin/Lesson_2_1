e4a3dc551446129bf5501b0027d6073d2e74ea55
Dmitry Rodin <dmrodin@bk.ru>
2022-03-02 10:17:49 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lesson_2_1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            const int value = 753;
+
+            int cnt = value;
+            int sum = 0;
+
+            while (cnt % 10 > 0)
+            {
+                sum += cnt % 10;
+                cnt /= 10;
+            }
+
+            Console.WriteLine(sum);
+            //Console.ReadKey();
+        }
+    }
+}
